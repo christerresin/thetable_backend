ad552dde0d720fcf46a37681f37a795afabfb2b6
Christerresin <christer.resin@gmail.com>
2023-01-03 15:03:54 +0100

    add Tests project and boilerplate folders

---
+++ TheTableBackend.Test/Controllers/AppetizerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheTableBackend.Controllers;
+
+namespace TheTableBackend.Test.Controllers
+{
+ 
+    public class AppetizerControllerTests
+    {
+        [Theory]
+        [InlineData(1, "Pikachu!")]
+        public void AppetizerController_pikaPika_ReturnString(int number, string expected)
+        {
+            // Arrange
+            var appetizerController = new AppetizerController();
+
+            // Act
+            var result = appetizerController.pikaPika(number);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ TheTableBackend/Controllers/AppetizerController.cs
@@ -0,0 +1,17 @@
+﻿namespace TheTableBackend.Controllers
+{
+    public class AppetizerController
+    {
+        public string pikaPika(int number)
+        {
+            if(number == 1)
+            {
+                return "Pikachu!";
+            }
+            else
+            {
+                return "Pokemon";
+            }
+        }
+    }
+}
